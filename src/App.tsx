dafa651d9f4688df43dc0c10d7c64c1ca5ec059c
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-01-30 01:17:40 +0600

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@material-tailwind/react"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>ppp</h1>
+      <Button placeholder={''} color="blue" >Submit</Button>
+    </>
+  )
+}
+
+export default App

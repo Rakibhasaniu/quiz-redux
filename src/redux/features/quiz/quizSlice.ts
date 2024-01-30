c90a59cd75efabe5a6c611609e096a0cde5601d4
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-01-30 17:29:35 +0600

    addQuiz slice created

---
+++ src/redux/features/quiz/quizSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type TQuiz = {
+    module: string;
+    question: string;
+    description: string;
+    options: string[];
+    correctOptions:string[];
+}
+type TInitialState = {
+    quiz:TQuiz[]
+}
+type TAction = {
+    payload: TQuiz;
+}
+const initialState :TInitialState = {
+    quiz:[],
+}
+
+const quizSlice = createSlice({
+    name:"quiz",
+    initialState,
+    reducers:{
+        addQuiz: (state,action: TAction) => {
+            // state.quiz = [...state.quiz,action.payload]
+            state.quiz.push(action.payload);
+        }
+    }
+})
